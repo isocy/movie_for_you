4266b252a40d1af83f38d28a8800231cef01f2d7
isocy <isocy13576@kaist.ac.kr>
2023-10-24 09:34:10 +0900

    Update & Add concatenation

--- job2_crawl_movie_review.py
+++ job2_crawl_movie_review.py
@@ -8,6 +8,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import *
 
 
@@ -25,6 +27,9 @@ options.add_argument('user_agent=' + user_agent)
 
 driver = webdriver.Chrome(service=service, options=options)
 
+if not os.path.isdir('./crawling_data/'):
+    os.mkdir('./crawling_data/')
+
 
 INITIAL_YEAR = 2020
 INITIAL_MONTH = 1
@@ -34,6 +39,7 @@ FINAL_MONTH = 9
 cur_year = INITIAL_YEAR
 cur_month = INITIAL_MONTH
 
+title_set = set()
 df_title_review = pd.DataFrame(columns=['title', 'review'])
 
 while cur_year != FINAL_YEAR or cur_month != FINAL_MONTH + 1:
@@ -47,34 +53,72 @@ while cur_year != FINAL_YEAR or cur_month != FINAL_MONTH + 1:
     for ranking_idx in range(1, 31):
         driver.get(url)
 
-        try:
-            movie_title_element = driver.find_element(
-                By.XPATH, '//*[@id="mainContent"]/div/div[2]/ol/li[{}]/div/div[2]/strong/a'.format(ranking_idx))
-        except NoSuchElementException:
-            print(cur_year, cur_month, ranking_idx)
-            break
+        movie_title_element = driver.find_element(
+            By.XPATH,
+            '//*[@id="mainContent"]/div/div[2]/ol/li[{}]/div/div[2]/strong/a'.format(ranking_idx)
+        )
         title = movie_title_element.text
+        
+        if title in title_set:
+            continue
+        else:
+            title_set.add(title)
+        if movie_title_element.get_attribute('href')[-1] == '=':
+            continue
+        
         movie_title_element.click()
         
-        review_tab_element = driver.find_element(By.XPATH, '//*[@id="mainContent"]/div/div[2]/div[1]/ul/li[4]/a/span')
+        while True:  # Infinite loop for page load
+            try:
+                driver.find_element(By.XPATH, '//*[@id="mainContent"]/div/div[2]/div[2]/div[1]/div/div/div')
+            except NoSuchElementException:
+                continue
+            break
+        review_tab_element = driver.find_element(By.XPATH, '//*[@id="mainContent"]/div/div[2]/div[1]/ul/li[4]/a')
         review_tab_element.click()
         
-        while True:  # Infinite loop for loading all reviews
-            try:
-                view_more_element = driver.find_element(
-                    By.XPATH, '//*[@id="alex-area"]/div/div/div/div[3]/div[1]/button')
-                view_more_element.click()
-            except NoSuchElementException:
-                break
-        
-        review_idx = 1
+        review_cnt_element = None
         while True:
             try:
-                review = driver.find_element(
-                    By.XPATH, '/html/body/div[2]/main/article/div/div[2]/div[2]/div/div/div[2]/div/div/div/div[3]/ul[2]'
-                              '/li[{}]/div/p'.format(review_idx))
+                review_cnt_element = driver.find_element(
+                    By.XPATH,
+                    '//*[@id="mainContent"]/div/div[2]/div[2]/div/strong/span'
+                )
             except NoSuchElementException:
+                continue
+            
+            if review_cnt_element.text:
                 break
+        review_cnt = int(re.compile('[^0-9]').sub('', review_cnt_element.text))
+        if review_cnt > 160:
+            review_cnt = 160
+        
+        for click_idx in range((review_cnt + 19) // 30):
+            view_more_element = None
+            while True:  # Infinite loop for button load
+                try:
+                    view_more_element = driver.find_element(
+                        By.XPATH,
+                        '//*[@id="alex-area"]/div/div/div/div[3]/div[1]/button'
+                    )
+                except NoSuchElementException:
+                    continue
+                
+                if not view_more_element.text == '':
+                    break
+            view_more_element.click()
+        
+        review_idx = 1
+        while review_idx <= review_cnt:
+            try:
+                review = driver.find_element(
+                    By.XPATH,
+                    '/html/body/div[2]/main/article/div/div[2]/div[2]/div/div/div[2]/div/div/div/div[3]/ul[2]'
+                    '/li[{}]/div/p'.format(review_idx)
+                ).text
+            except NoSuchElementException:
+                review_idx += 1
+                continue
             refined_review = re.compile('[^가-힣]').sub(' ', review)
             
             titles.append(title)
@@ -84,6 +128,10 @@ while cur_year != FINAL_YEAR or cur_month != FINAL_MONTH + 1:
 
     df_temp_title_review['title'] = titles
     df_temp_title_review['review'] = reviews
+    df_temp_title_review.to_csv(
+        './crawling_data/temp_title_review_{}{:02d}.csv'.format(cur_year, cur_month),
+        index=False
+    )
     df_title_review = pd.concat([df_title_review, df_temp_title_review], axis='rows', ignore_index=True)
 
     if cur_month == 12:
@@ -92,8 +140,8 @@ while cur_year != FINAL_YEAR or cur_month != FINAL_MONTH + 1:
     else:
         cur_month += 1
 
-if not os.path.isdir('./crawling_data/'):
-    os.mkdir('./crawling_data/')
-
-df_title_review.to_csv('./crawling_data/title_review_{}{:02d}-{}{:02d}.csv'
-                       .format(INITIAL_YEAR, INITIAL_MONTH, FINAL_YEAR, FINAL_MONTH))
+df_title_review.to_csv(
+    './crawling_data/title_review_{}{:02d}-{}{:02d}.csv'
+    .format(INITIAL_YEAR, INITIAL_MONTH, FINAL_YEAR, FINAL_MONTH),
+    index=False
+)
---
+++ job3_concat_csvs.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+
+df_title_review = pd.concat([
+    pd.read_csv('./crawling_data/title_review_201310-201612.csv'),
+    pd.read_csv('./crawling_data/title_review_201701-201912.csv'),
+    pd.read_csv('./crawling_data/title_review_202001-202309.csv')
+], axis='rows', ignore_index=True).drop_duplicates()
+
+df_title_review.to_csv('./crawling_data/title_review.csv', index=False)
